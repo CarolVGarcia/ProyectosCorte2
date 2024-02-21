921fb45fd84adb039a3b667cd658ab92256386ff
Juan Tirado <xxrektyouxx@gmail.com>
2024-02-21 07:16:06 -0700

    Prueba1

---
+++ router/index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import json from 'body-parser';
+
+export const router = express.Router();
+
+router.get('/', (req, res)=>{
+
+    res.render('index',{titulo:"Mis practicas js", nombre:"Vazquez Garcia Carol"})
+
+
+})
+
+router.get('/tabla',(req,res)=>{
+    //parametros
+    const params = {
+        numero:req.query.numero
+    }
+    res.render ('tabla',params)
+})
+
+router.post('/tabla',(req,res)=>{
+    //parametros
+    const params = {
+        numero:req.body.numero 
+    }
+    res.render ('tabla',params)
+})
+
+
+
+
+
+
+
+
+
+
+
+export default {router}
+
